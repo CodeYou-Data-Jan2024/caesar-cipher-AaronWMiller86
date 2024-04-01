9d90d69110e686470b3ebdfb4e64433db7e9ae9b
Aaron W Miller <aaronwmiller86@gmail.com>
2024-04-01 15:37:13 -0400

    Tweaked code to allow for easier stepping amount alterations in the future.

--- cipher.py
+++ cipher.py
@@ -1,12 +1,15 @@
+cipher_amount = 5
+correction_ammount = 26 - cipher_amount
+
 def ceasar(text):
     result = ""
     for letter in text:
         if letter.isalpha():
-            if chr(ord(letter) + 5).isalpha():
-                unicode = ord(letter) + 5
+            if chr(ord(letter) + cipher_amount).isalpha():
+                unicode = ord(letter) + cipher_amount
                 result += chr(unicode)
             else:
-                unicode = ord(letter) - 21
+                unicode = ord(letter) - correction_ammount
                 result += chr(unicode)
         else:
             result += letter
