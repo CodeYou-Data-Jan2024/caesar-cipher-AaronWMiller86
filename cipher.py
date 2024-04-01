c96f456ee0e2ebbb5f5a0647851ffc0bc369f47d
Aaron W Miller <aaronwmiller86@gmail.com>
2024-04-01 15:27:14 -0400

    Fixed letters rolling over into special chatacters.

--- cipher.py
+++ cipher.py
@@ -2,8 +2,12 @@ def ceasar(text):
     result = ""
     for letter in text:
         if letter.isalpha():
-            unicode = ord(letter) + 5
-            result += chr(unicode)
+            if chr(ord(letter) + 5).isalpha():
+                unicode = ord(letter) + 5
+                result += chr(unicode)
+            else:
+                unicode = ord(letter) - 21
+                result += chr(unicode)
         else:
             result += letter
     print("Your cipher text is: ")
